f58e282c8ac09ce8e8aed6812da725049ea5cbb6
Anele Aneal Tose <216079292@mycput.ac.za>
2021-04-01 20:15:20 +0200

    Here i commit all the changes i've done so far

--- src/main/java/za/ac/cput/adp2assignment1/Dog.java
+++ src/main/java/za/ac/cput/adp2assignment1/Dog.java
@@ -8,5 +8,35 @@ package za.ac.cput.adp2assignment1;
  * Date: 01 April 2021
  */
 public class Dog {
+    private String dogName, dogType, dogColor;
     
+    @Override
+    public String toString() {
+        return "Dog{" + "dogName=" + dogName + ", dogType=" + dogType + ", dogColor=" + dogColor + '}';
+    }
+    
+    
+    public void setDogName(String dogName) {
+        this.dogName = dogName;
+    }
+
+    public void setDogType(String dogType) {
+        this.dogType = dogType;
+    }
+
+    public void setDogColor(String dogColor) {
+        this.dogColor = dogColor;
+    }
+
+    public String getDogName() {
+        return dogName;
+    }
+
+    public String getDogType() {
+        return dogType;
+    }
+
+    public String getDogColor() {
+        return dogColor;
+    }
 }
