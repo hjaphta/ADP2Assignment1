9c7bc4aac0948e8eefd3843338de833419ce9daf
marcia319 <Marcia Zanele Bika@LAPTOP-02JKT4IR>
2021-04-01 20:12:25 +0200

    Added a Main class that adds 2 numbers

---
+++ src/test/java/za/ac/cput/adp2assignment1/MainTest.java
@@ -0,0 +1,38 @@
+package za.ac.cput.adp2assignment1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+@Disabled("program disabled ")
+public class MainTest{
+public MainTest(){
+
+@Before
+int x = 10;
+System.out.println(x == 10)
+
+@Test
+void testIdentity(){
+    assertSame(x){
+}
+
+@Test
+void testEquality(){
+    assertEquals(x){
+}
+
+@Test
+void testGetId(){
+    System.out.println("get Id");
+    
+}
+}
+}
+
+}
+}
+
