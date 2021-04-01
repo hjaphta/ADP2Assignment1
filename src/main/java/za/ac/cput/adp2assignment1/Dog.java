596e9c3fdb0f76db22eee75d587e2c456866389a
Anele Aneal Tose <216079292@mycput.ac.za>
2021-04-01 17:52:04 +0200

    This is my first commit, here i added a class call Dog with some comments

---
+++ src/main/java/za/ac/cput/adp2assignment1/Dog.java
@@ -0,0 +1,12 @@
+
+package za.ac.cput.adp2assignment1;
+
+/**
+ * Dog.java
+ * This class i'm going to use to implement TDD features
+ * @author Anele Aneal Tose - 216079292
+ * Date: 01 April 2021
+ */
+public class Dog {
+    
+}
