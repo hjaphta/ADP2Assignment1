3b37da57a7b1ebe4587d9fab0d2bb24863d38b05
Zintle <218109911@mycput.ac.za>
2021-04-01 11:19:29 +0200

    Added methods in Book() Added method in BookTest()

--- src/main/java/za/ac/cput/adp2assignment1/Book.java
+++ src/main/java/za/ac/cput/adp2assignment1/Book.java
@@ -1,14 +1,58 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Maven Project
+ *Book.java
+  *Worker class
+ *Author Zintle Magwaxaza-218109911
+ *Date: Wednesday, 31 March 2021
  */
 package za.ac.cput.adp2assignment1;
 
-/**
- *
- * @author BOITUMELO ASANDA NKG
- */
 public class Book {
+    private String bookTitle,bookId,bookGenre,date;
+    
+    public Book(String bookTitle, String bookId, String bookGenre, String date) {
+        this.bookTitle = bookTitle;
+        this.bookId = bookId;
+        this.bookGenre = bookGenre;
+        this.date = date;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookGenre() {
+        return bookGenre;
+    }
+
+    public void setBookGenre(String bookType) {
+        this.bookGenre = bookType;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "bookTitle=" + bookTitle + ", bookId=" + bookId + ", bookGenre=" + bookGenre + ", date=" + date + '}';
+    }
+
     
 }
--- src/test/java/za/ac/cput/adp2assignment1/BookTest.java
+++ src/test/java/za/ac/cput/adp2assignment1/BookTest.java
@@ -1,42 +1,58 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+  *Maven Project
+  *BookTest.java
+  *Working class
+  *Author Zintle Magwaxaza-218109911
+  *Date: Wednesday, 31 March 2021
  */
 package za.ac.cput.adp2assignment1;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author BOITUMELO ASANDA NKG
- */
+
 public class BookTest {
-    
-    public BookTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
+    private Book book0;
+     private Book book1;
+      private Book book2;
+       private Book book3;
+       
     
     @BeforeEach
     public void setUp() {
+        book0 = new Book ("Star Wars", "NO678", "Action","June 2020");
+        book1 = new Book("After We Collided", "NO214", "Romance","March 2020");
+        book2 = new Book("World History", "NO258", "Action","Jan 2021");
+        book0=book3;
+        
     }
+    
+    //Equality
+    @Test
+    public void testEquality(){
+        assertEquals(book0,book3);
+    }
+    
+    //Identity
+    @Test
+    public void TestIdentity(){
+        assertSame(book0,book3);
+    }
+    
+   //fail
+    @Test
+    public void testFail() {
+        assertEquals(book0,book3);
+        fail("This test will fail");
+    }
+    
     
     @AfterEach
     public void tearDown() {
     }
-
+    
     @Test
     public void testSomeMethod() {
         // TODO review the generated test code and remove the default call to fail.
